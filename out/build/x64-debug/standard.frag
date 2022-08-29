3602877f27e9a0ec4d91503f5ec2966d4c006cbb
Ratchgamer123 <trautwein.maximilian04@gmail.com>
2022-08-30 01:07:01 +0200

    almost finished

--- out/build/x64-debug/standard.frag
+++ out/build/x64-debug/standard.frag
@@ -1,6 +1,9 @@
 #version 330 core
 out vec4 FragColor;
+
+uniform vec4 currentColor;
+
 void main()
 {
-   FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+   FragColor = currentColor;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
-﻿#include <iostream>
+﻿#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+
+#include <iostream>
+#include <vector>
+#include <random>
+#include <cmath>
 
 #include "glad.h"
 #include "glfw3.h"
@@ -8,10 +13,123 @@
 
 #include "shader.h"
 
-int screenWidth = 1024, screenHeight = 576;
+#define PI 3.14159
+
+struct Vertex
+{
+	float x, y;
+};
+
+int screenWidth = 1000, screenHeight = 1000;
+
+float distanceThreshold = 0.5f;
+int points = 5;
+float pointScale = 10.0f;
+
+bool greenCircleActive = true;
+bool redCirclesActive = true;
+
+bool lastRed = false;
+
+std::vector<Vertex> redPoints;
+std::vector<Vertex> bluePoints;
+std::vector<Vertex> circlePoints;
+std::vector<Vertex> sqrtcirclePoints;
+
+std::random_device rd;
+std::default_random_engine gen(rd());
+
+void renderCircles()
+{
+	for (int i = 0; i < redPoints.size(); i++)
+	{
+		Vertex currentRedVertex = redPoints.at(i);
+
+		for (float angle = 0.0f; angle <= (2.0f * PI); angle += 0.01f)
+		{
+			Vertex currentVertex = Vertex(currentRedVertex.x + distanceThreshold * sin(angle), currentRedVertex.y + distanceThreshold * cos(angle));
+			circlePoints.push_back(currentVertex);
+		}
+	}
+}
+
+void rendersqrtCircle()
+{
+	for (float angle = 0.0f; angle <= (2.0f * PI); angle += 0.01f)
+	{
+		Vertex currentVertex = Vertex((distanceThreshold * (sqrt(2) / 2)) * sin(angle), (distanceThreshold * (sqrt(2) / 2)) * cos(angle));
+		sqrtcirclePoints.push_back(currentVertex);
+	}
+}
+
+float randFloat(float low, float high)
+{
+	std::uniform_real_distribution<float> dist(low, high);
+	return dist(gen);
+}
+
+void checkBluePointThreshold()
+{
+	float currentBlueX = randFloat(-1.0f, 1.0f);
+	float currentBlueY = randFloat(-1.0f, 1.0f);
+
+	for (int i = 0; i < redPoints.size(); i++)
+	{
+		Vertex currentRedVertex = redPoints.at(i);
+		if (sqrt(pow((currentRedVertex.x - currentBlueX), 2.0f) + pow((currentRedVertex.y - currentBlueY), 2.0f)) > distanceThreshold)
+		{
+			checkBluePointThreshold();
+			return;
+		}
+	}
+
+	bluePoints.push_back(Vertex(currentBlueX, currentBlueY));
+}
+
+void checkRedPointThreshold()
+{
+	float currentRedX = randFloat(-1.0f, 1.0f);
+	float currentRedY = randFloat(-1.0f, 1.0f);
+
+	for (int i = 0; i < bluePoints.size(); i++)
+	{
+		Vertex currentBlueVertex = bluePoints.at(i);
+		if (sqrt(pow((currentBlueVertex.x - currentRedX), 2.0f) + pow((currentBlueVertex.y - currentRedY), 2.0f)) > distanceThreshold)
+		{
+			checkRedPointThreshold();
+			return;
+		}
+	}
+
+	redPoints.push_back(Vertex(currentRedX, currentRedY));
+}
+
+void calculatePoints()
+{
+	//redPoints.push_back(Vertex(randFloat(-1.0f, 1.0f), randFloat(-1.0f, 1.0f)));
+	lastRed = true;
+
+	for (int i = 0; i < points; i++)
+	{
+		if (lastRed)
+		{
+			checkBluePointThreshold();
+			lastRed = false;
+		}
+		else
+		{
+			checkRedPointThreshold();
+			lastRed = true;
+		}
+	}
+}
 
 int main()
 {
+	calculatePoints();
+	renderCircles();
+	rendersqrtCircle();
+
 	glfwInit();
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -31,13 +149,6 @@ int main()
 
 	gladLoadGL();
 
-	GLfloat vertices[] =
-	{
-		-0.5f, -0.5f * float(sqrt(3)) / 3, // Lower left corner
-		0.5f, -0.5f * float(sqrt(3)) / 3, // Lower right corner
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3 // Upper corner
-	};
-
 	glViewport(0, 0, screenWidth, screenHeight);
 
 	IMGUI_CHECKVERSION();
@@ -50,17 +161,65 @@ int main()
 
 	Shader mainShader("standard.vert", "standard.frag");
 
-	GLuint vao, vbo;
+	GLuint redVAO, redVBO, blueVAO, blueVBO, circVAO, circVBO, sqrtcircVAO, sqrtcircVBO;
 
-	glGenVertexArrays(1, &vao);
-	glGenBuffers(1, &vbo);
+	glGenVertexArrays(1, &redVAO);
+	glGenBuffers(1, &redVBO);
 
-	glBindVertexArray(vao);
+	glBindVertexArray(redVAO);
 
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, redVBO);
+	glBufferData(GL_ARRAY_BUFFER, redPoints.size() * sizeof(Vertex), redPoints.data(), GL_DYNAMIC_DRAW);
 
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	//
+
+	glGenVertexArrays(1, &blueVAO);
+	glGenBuffers(1, &blueVBO);
+
+	glBindVertexArray(blueVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, blueVBO);
+	glBufferData(GL_ARRAY_BUFFER, bluePoints.size() * sizeof(Vertex), bluePoints.data(), GL_DYNAMIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	//
+
+	glGenVertexArrays(1, &circVAO);
+	glGenBuffers(1, &circVBO);
+
+	glBindVertexArray(circVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, circVBO);
+	glBufferData(GL_ARRAY_BUFFER, circlePoints.size() * sizeof(Vertex), circlePoints.data(), GL_DYNAMIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	//
+
+	glGenVertexArrays(1, &sqrtcircVAO);
+	glGenBuffers(1, &sqrtcircVBO);
+
+	glBindVertexArray(sqrtcircVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, sqrtcircVBO);
+	glBufferData(GL_ARRAY_BUFFER, sqrtcirclePoints.size() * sizeof(Vertex), sqrtcirclePoints.data(), GL_DYNAMIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -79,17 +238,81 @@ int main()
 
 		mainShader.activate();
 
-		glUniform1f(glGetUniformLocation(mainShader.id, "pointSize"), 2.0f);
+		glUniform1f(glGetUniformLocation(mainShader.id, "pointSize"), pointScale);
 
-		glBindVertexArray(vao);
-		glDrawArrays(GL_POINTS, 0, 3);
+		glUniform4f(glGetUniformLocation(mainShader.id, "currentColor"), 1.0f, 0.0f, 0.0f, 1.0f);
+		glUniform1f(glGetUniformLocation(mainShader.id, "pointSize"), pointScale / 2);
+
+		glBindVertexArray(redVAO);
+		glDrawArrays(GL_POINTS, 0, redPoints.size());
+
+		glUniform4f(glGetUniformLocation(mainShader.id, "currentColor"), 0.0f, 0.0f, 1.0f, 1.0f);
+
+		glBindVertexArray(blueVAO);
+		glDrawArrays(GL_POINTS, 0, bluePoints.size());
+
+		if (greenCircleActive)
+		{
+			glUniform4f(glGetUniformLocation(mainShader.id, "currentColor"), 0.0f, 1.0f, 0.0f, 1.0f);
+
+			glBindVertexArray(sqrtcircVAO);
+			glDrawArrays(GL_POINTS, 0, sqrtcirclePoints.size());
+		}
+		if (redCirclesActive)
+		{
+			glUniform4f(glGetUniformLocation(mainShader.id, "currentColor"), 1.0f, 0.0f, 0.0f, 1.0f);
+
+			glBindVertexArray(circVAO);
+			glDrawArrays(GL_POINTS, 0, circlePoints.size());
+		}
 
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
 		ImGui::Begin("Lionel hat einen fetten Schwanz");
-		ImGui::Button("TRUE");
+		ImGui::InputInt("Punkte", &points);
+		ImGui::InputFloat("Größe", &pointScale);
+		ImGui::InputFloat("Maximale Distanz", &distanceThreshold);
+		if (ImGui::RadioButton("Rote Kreise", redCirclesActive))
+		{
+			if (redCirclesActive)
+			{
+				redCirclesActive = false;
+			}
+			else
+			{
+				redCirclesActive = true;
+			}
+		}
+		if (ImGui::RadioButton("Wurzel 2 Kreis", greenCircleActive))
+		{
+			if (greenCircleActive)
+			{
+				greenCircleActive = false;
+			}
+			else
+			{
+				greenCircleActive = true;
+			}
+		}
+		if (ImGui::Button("Generieren"))
+		{
+			redPoints.clear();
+			bluePoints.clear();
+			circlePoints.clear();
+			calculatePoints();
+			renderCircles();
+			rendersqrtCircle();
+			glBindBuffer(GL_ARRAY_BUFFER, redVBO);
+			glBufferData(GL_ARRAY_BUFFER, redPoints.size() * sizeof(Vertex), redPoints.data(), GL_DYNAMIC_DRAW);
+			glBindBuffer(GL_ARRAY_BUFFER, blueVBO);
+			glBufferData(GL_ARRAY_BUFFER, bluePoints.size() * sizeof(Vertex), bluePoints.data(), GL_DYNAMIC_DRAW);
+			glBindBuffer(GL_ARRAY_BUFFER, circVBO);
+			glBufferData(GL_ARRAY_BUFFER, circlePoints.size() * sizeof(Vertex), circlePoints.data(), GL_DYNAMIC_DRAW);
+			glBindBuffer(GL_ARRAY_BUFFER, sqrtcircVBO);
+			glBufferData(GL_ARRAY_BUFFER, sqrtcirclePoints.size() * sizeof(Vertex), sqrtcirclePoints.data(), GL_DYNAMIC_DRAW);
+		}
 		ImGui::End();
 
 		ImGui::Render();
@@ -99,8 +322,11 @@ int main()
 	}
 
 	mainShader.dispose();
-	glDeleteVertexArrays(1, &vao);
-	glDeleteBuffers(1, &vbo);
+	glDeleteVertexArrays(1, &redVAO);
+	glDeleteBuffers(1, &redVBO);
+
+	glDeleteVertexArrays(1, &blueVAO);
+	glDeleteBuffers(1, &blueVBO);
 
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
